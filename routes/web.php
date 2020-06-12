dc8a862ece96ad96d6889ec32585efaa4d21934e
dpjhurley <dpjhurley@gmail.com>
2020-06-12 16:06:59 +0200

    edits/update

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,23 @@ Route::get('/pets', 'PetController@index')->name('pets.index');
 Route::get('/pets/{pet_id}', 'PetController@show')->where('pet_id','[0-9]+')->name('pet_id');
 Route::post('/pets', 'PetController@index');
 
+Route::get( '/pets/create',    'PetController@create')  ->name('pets.create');
+Route::post('/pets',           'PetController@store')   ->name('pets.store');
+Route::get( '/pets/{id}/edit', 'PetController@edit')    ->name('pets.edit');
+Route::put( '/pets/{id}',      'PetController@update')  ->name('pets.update');
 
-
+//-------------------------------------------------------------------------------------------
 
 Route::get('/owners', 'OwnerController@index')->name('owners.index');
 Route::get('/owners/{id}', 'OwnerController@show')->where('id','[0-9]+')->name('owner_id');
 Route::post('/owners', 'OwnerController@index')->name('owners.index');
+
+Route::get( '/owners/create',    'OwnerController@create')  ->name('owners.create');
+Route::post('/owners',           'OwnerController@store')   ->name('owners.store');
+Route::get( '/owners/{id}/edit', 'OwnerController@edit')    ->name('owners.edit');
+Route::put( '/owners/{id}',      'OwnerController@update')  ->name('owners.update');
+
+//-------------------------------------------------------------------------------------------
 
 Route::get('/doctor', 'DoctorController@index')->name('doctor.index');
 
